279852e6dd8574ece7a51afb49349edb60d44f8a
Juan Feris <feris.juan@gmail.com>
2020-10-05 16:32:15 -0500

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const { check, validationResult } = require('express-validator/check')
+const gravatar = require('gravatar')
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/User')
 
 // @route   GET api/users
 // @desc    Register user
@@ -17,14 +21,51 @@ router.post(
 			min: 6,
 		}),
 	],
-	(req, res) => {
+	async (req, res) => {
 		const errors = validationResult(req)
 		if (!errors.isEmpty()) {
 			return res.status(400).json({
 				errors: errors.array(),
 			})
 		}
-		res.send('Users route')
+
+		const { name, email, password } = req.body
+
+		try {
+			// See if the user exists
+			let user = await User.findOne({ email })
+
+			if (user) {
+				return res.status(400).json({
+					errors: [{ msg: 'User already exists' }],
+				})
+			}
+
+			// Get users gravatar
+			const avatar = gravatar.url(email, {
+				s: '200',
+				r: 'pg',
+				d: 'mm',
+			})
+
+			user = new User({ name, email, avatar, password })
+
+			// Encrypt password with bcrypt
+			const salt = await bcrypt.genSalt(10)
+			user.password = await bcrypt.hash(password, salt)
+
+			// Save to DB
+			await user.save()
+			res.send('User registered')
+
+			// Return jwt
+
+			res.send('Users route')
+		} catch (err) {
+			console.log(err.message)
+
+			res.status(500).send('Server error')
+		}
 	}
 )
 
