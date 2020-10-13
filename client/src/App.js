85930c7df9aa16cbb0cb07d57df1a280b5ac943c
Juan Feris <feris.juan@gmail.com>
2020-10-13 18:01:46 -0500

    Basic Landing Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Landing from './components/layout/Landing'
+import Navbar from './components/layout/Navbar'
+
+const App = () => {
+	return (
+		<>
+			<Navbar />
+			<Landing />
+		</>
+	)
 }
 
-export default App;
+export default App
