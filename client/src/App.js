a56721d96bfa785f8bd85bc09978af24804003bf
Juan Feris <feris.juan@gmail.com>
2020-10-16 13:19:56 -0500

    Made PrivateRoute

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,11 @@ import './App.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Alert from './components/layout/Alert'
+import Dashboard from './components/dashboard/Dashboard'
 import Landing from './components/layout/Landing'
 import Login from './components/auth/Login'
 import Navbar from './components/layout/Navbar'
+import PrivateRoute from './components/routing/PrivateRote'
 
 // Redux
 import { Provider } from 'react-redux'
@@ -29,6 +31,7 @@ const App = () => {
 						<Switch>
 							<Route exact path='/register' component={Register} />
 							<Route exact path='/login' component={Login} />
+							<PrivateRoute exact path='/dashboard' component={Dashboard} />
 						</Switch>
 					</section>
 				</>
---
+++ client/src/components/routing/PrivateRote.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { Route, Redirect } from 'react-router-dom'
+import PropTypes from 'prop-types'
+import auth from '../../reducers/auth'
+
+const PrivateRote = ({
+	component: Component,
+	auth: { isAuthN, loading },
+	...rest
+}) => (
+	<Route
+		{...rest}
+		render={props =>
+			!isAuthN && !loading ? <Redirect to='/login' /> : <Component {...props} />
+		}
+	/>
+)
+
+PrivateRote.propTypes = {
+	auth: PropTypes.object.isRequired,
+}
+
+const mapStateToProps = state => ({
+	auth: state.auth,
+})
+
+export default connect(mapStateToProps)(PrivateRote)
